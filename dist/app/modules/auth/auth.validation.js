5073a2fd102b5633190a965b28ffe33b1824bf18
Nihar Mondal <developernihar@gmail.com>
2024-12-27 22:54:48 +0600

    updated: build folder

--- dist/app/modules/auth/auth.validation.js
+++ dist/app/modules/auth/auth.validation.js
@@ -1,1 +1,15 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.authValidation = void 0;
+const zod_1 = require("zod");
+const resetPassword = zod_1.z.object({
+    password: zod_1.z
+        .string({ required_error: "Password is a required field" })
+        .trim(),
+    confirmPassword: zod_1.z
+        .string({
+        required_error: "Confirm password is a required field",
+    })
+        .trim(),
+});
+exports.authValidation = { resetPassword };
---
+++ dist/app/utils/sendEmail.js
@@ -0,0 +1,43 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sendEmail = void 0;
+const nodemailer_1 = __importDefault(require("nodemailer"));
+const config_1 = require("../../config");
+const sendEmail = (to, link) => __awaiter(void 0, void 0, void 0, function* () {
+    const transporter = nodemailer_1.default.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false, // true for port 465, false for other ports
+        auth: {
+            user: config_1.envConfig.emailUtils.email,
+            pass: config_1.envConfig.emailUtils.password,
+        },
+    });
+    const info = yield transporter.sendMail({
+        from: config_1.envConfig.emailUtils.email, // sender address
+        to, // list of receivers
+        subject: "Reset Trip Mate password", // Subject line
+        html: `
+			<div>
+				<p style='margin-bottom: "20px"'>
+					<b>Reset your password within 10 minutes</b>
+				</p>
+				<a href=${link} >Click here to reset your password</a>
+			</div>
+		`,
+    });
+    return info === null || info === void 0 ? void 0 : info.messageId;
+});
+exports.sendEmail = sendEmail;
